3b68399b31b4758d0c16b4d38ddffba343f474e3
saniyaforgit <saniyaforgit@gmail.com.com>
2026-03-20 13:06:14 +0500

    first commit

--- lesson4/lesson6_Grid/lesson7/homework.display flex/practice/homework.display flex2/project/JSLESSONS/secondlesson/app.js
+++ lesson4/lesson6_Grid/lesson7/homework.display flex/practice/homework.display flex2/project/JSLESSONS/secondlesson/app.js
@@ -50,7 +50,80 @@
 //     console.log(i)
 // }
 
-for (let i =1; i<10; i++){
-    sum +=i
+// for (let i =1; i<10; i++){
+//     sum +=i
+// }
+// console.log(sum)
+
+// let i =1
+// do {
+//     console.log(i)
+//     i++
+// }
+// while(false)
+
+// let choice
+
+// do{
+//     choice = prompt("1 - привет, 2- выход")
+//     if(choice =="1")
+// }
+
+// let choicedo{
+//     choice=prompt("1-Привет, 2- Выход")
+//     if (choice === "1"){
+//         console.log("Привет")
+//     }
+// }
+// while (choice !=="2")
+
+
+// for (let i=1; i<=3; i++){
+//     for (let j =1; j<=3; j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for (int i=1; i <= 3; i++){
+//     let row =""
+
+//     for (let j=1; j <=3; j++){
+//         row += "*"
+//     }
+//     console.log (row)
+// }
+
+// for (let i=1; <=5; i++){
+//     let row =""
+//     for (let j =1; j<=i; j++){
+//         row +="*"
+//     }
+//     console.log(row)
+// }
+
+// for (let i= 0; i<8; i++){
+//     let row =""
+//     for (let j=0; j<8; j++){
+//         if ((i=j)%2===0){
+//             row+="⬛"
+//         }
+//         else{
+//             row +="⬜"
+//         }
+//     }
+//     console.log (row)
+// }
+
+let i =1
+while (i<=10){
+    if (i % 2!==0){
+        console.log(i)
+    }
+    i ++    
 }
-console.log(sum)
+
+for (let i =1; i <=10; i++){
+    if (i %3 === 0){
+        console.log(i)
+    }
+}
